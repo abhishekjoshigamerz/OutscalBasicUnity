c226ad4279f39a5a6764db44de5707ad0e88a1cc
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-09-14 22:34:59 +0530

    Commit till Chapter5 Will Commit more in morning!

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    void Awake() {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log("This is Debugging!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.F)){
+          Debug.Log("F has been pressed down");
+      }
+      if(Input.GetKey(KeyCode.F)){
+          Debug.Log("F has been pressed ");
+      }
+      if(Input.GetKeyUp(KeyCode.F)){
+           Debug.Log("F has been released ");
+      }
+      if(Input.GetMouseButtonDown(0)){
+           Debug.Log("LMB has been pressed ");
+      }
+      
+    }
+    void LateUpdate() {
+        
+    }
+
+    private void OnDestroy() {
+        
+    }
+}
