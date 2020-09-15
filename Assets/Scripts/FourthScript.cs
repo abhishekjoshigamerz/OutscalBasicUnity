46c62859eeb3d43c63b17954d91c2d587259f64d
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-09-15 12:15:50 +0530

    Final Commit for Assignments!

---
+++ Assets/Scripts/FourthScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class FourthScript : MonoBehaviour
+{
+    
+    public Button button;
+    public string newScene;
+     public Button masterbutton;
+    private string masterscene="MasterScene";
+    void Start()
+    {
+        button.onClick.AddListener(ChangeScene);
+        masterbutton.onClick.AddListener(OnMasterClick);
+    }
+
+    // Update is called once per frame
+    void   ChangeScene(){
+        SceneManager.LoadScene(newScene);
+    }
+     private void OnMasterClick(){
+        SceneManager.LoadScene(masterscene);
+    }
+}
---
+++ Assets/Scripts/MySecondScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MySecondScript : MonoBehaviour
+{
+    public Button button;
+   
+    public string newScene;
+     public Button masterbutton;
+    private string masterscene="MasterScene";
+    private void Start(){
+        button.onClick.AddListener(OnButtonClick);
+        masterbutton.onClick.AddListener(OnMasterClick);
+    }
+    private void OnButtonClick(){
+        Debug.Log("Button Clicked!");
+        SceneManager.LoadScene(newScene);
+    }
+    private void OnMasterClick(){
+        SceneManager.LoadScene(masterscene);
+    }
+   
+}
