a7c9b7b21cd57fb13cce2c64752327df53a0f953
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-09-19 21:24:18 +0530

    Commit single file into featurebuttom branch

---
+++ Assets/Scripts/SceneChangeScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChangeScript : MonoBehaviour
+{
+
+    //Using Event Triggers on Unity to switch between Scenes
+    public void SceneLoading(int SceneNumber){
+        SceneManager.LoadScene(SceneNumber);
+    }
+}
